61406eb5b1c750027ba8f99cf9c669da56d3e724
Tony Bowden <tony@tmtm.com>
2022-04-06 18:59:31 +0300

    Switch to nullless diffs

--- bin/diff.rb
+++ bin/diff.rb
@@ -3,6 +3,6 @@
 
 require 'every_politician_scraper/comparison'
 
-diff = EveryPoliticianScraper::Comparison.new('data/wikidata.csv', 'data/official.csv').diff
+diff = EveryPoliticianScraper::NulllessComparison.new('data/wikidata.csv', 'data/official.csv').diff
                                          .reject { |row| row.last.to_s.include? 'Minister of State' }
 puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
